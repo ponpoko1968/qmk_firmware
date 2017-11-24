1fea4374cb420721e9ed470ab464f8a353c49f80
Shuji OCHI <ponpoko1968@gmail.com>
2017-11-24 20:51:34 +0900

    左親指のCMDとCtrlキーの順番を逆転

--- keyboards/ergodox_ez/keymaps/qvgnxx/keymap.c
+++ keyboards/ergodox_ez/keymaps/qvgnxx/keymap.c
@@ -20,27 +20,52 @@ enum custom_keycodes {
   RGB_800080
 };
 
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Keymap 0: Basic layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |  Esc   |   1  |   2  |   3  |   4  |   5  | DEL  |           |      |   6  |   7  |   8  |   9  |   0  |  BS    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   -    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | Tab    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |;     | Enter  |
+ * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |/     | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | Ctrl |      |      |  Alt |Shift |                                       |Shift|      |   Up |  Dn  | TG3  |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | <    |   >  |       | Home | Alt    |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | PgUp |       | End  |        |      |
+ *                                 | Cmd  |Ctrl  |------|       |------|  OSL1  |Enter |
+ *                                 |      |      | PgDn |       | Tab  |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+
 
   [0] = KEYMAP(
                /* 左手 */
-               KC_ESCAPE      , KC_1, KC_2, KC_3, KC_4, KC_5, KC_DELETE, /* 7 */
-               KC_TAB         , KC_Q, KC_W, KC_E, KC_R, KC_T, LT(1,      KC_NO), /* 7 */
-               KC_TRANSPARENT , KC_A, KC_S, KC_D, KC_F, KC_G, /* 6 */
-               KC_LSHIFT      , KC_Z, KC_X, KC_C, KC_V, KC_B, LT(2,      KC_NO), /* 7 */
+               KC_ESCAPE,      KC_1, KC_2, KC_3, KC_4, KC_5, KC_DELETE, /* 7 */
+               KC_TRANSPARENT, KC_Q, KC_W, KC_E, KC_R, KC_T, LT(1,      KC_NO), /* 7 */
+               KC_TAB,         KC_A, KC_S, KC_D, KC_F, KC_G, /* 6 */
+               KC_LSHIFT,      KC_Z, KC_X, KC_C, KC_V, KC_B, LT(2,      KC_NO), /* 7 */
 
+               KC_LCTL,KC_TRANSPARENT,KC_TRANSPARENT,KC_LALT,KC_LSHIFT, /*  */
 
-               KC_LCTL,KC_TRANSPARENT,KC_TRANSPARENT,KC_LALT,KC_LGUI, /*  */
                KC_LEFT,KC_RIGHT,
                KC_PGUP,
-               KC_LGUI,KC_LSHIFT,KC_PGDOWN,
+               KC_LGUI,KC_LCTL,KC_PGDOWN,
 
                /* 右手 */
                KC_TRANSPARENT, KC_6, KC_7, KC_8,     KC_9,      KC_0,     KC_BSPACE,
                TG(1),          KC_Y, KC_U, KC_I,     KC_O,      KC_P,     KC_MINUS,
                KC_H,           KC_J, KC_K, KC_L,     KC_SCOLON, KC_ENTER,
                TG(2),          KC_N, KC_M, KC_COMMA, KC_DOT,    KC_SLASH, KC_RSHIFT,
-               KC_LSHIFT,   KC_TRANSPARENT,      KC_UP, KC_DOWN, TG(3),
+               KC_RSHIFT,   KC_TRANSPARENT,      KC_UP, KC_DOWN, TG(3),
                KC_HOME, KC_LALT,
                KC_END,
                KC_TAB,  OSL(1), KC_SPACE),
