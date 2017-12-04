47caec8f81eacc3e69e5fbe43892fab82d73bfd9
Shuji OCHI <ponpoko1968@gmail.com>
2017-12-04 20:36:09 +0900

    capslockの位置をC-]（spacemacsルート ）に設定   LCTLとLGUIを入れ替えて元通りに

--- keyboards/ergodox_ez/keymaps/qvgnxx/keymap.c
+++ keyboards/ergodox_ez/keymaps/qvgnxx/keymap.c
@@ -49,16 +49,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = KEYMAP(
                /* 左手 */
-               KC_ESCAPE,      KC_1,     KC_2,      KC_3,      KC_4,      KC_5,     KC_DELETE,
-               KC_TAB,         KC_Q,     KC_W,      KC_E,      KC_R,      KC_T,     LT(1,      KC_NO),
-               KC_BSPACE,      KC_A,     KC_S,      KC_D,      KC_F,      MT(MOD_LCTL|MOD_LGUI, KC_G),
-               KC_LSHIFT,      KC_Z,     KC_X,      KC_C,      KC_V,      KC_B,     LT(2,      KC_NO),
+               KC_ESCAPE,         KC_1, KC_2, KC_3, KC_4, KC_5,                 KC_DELETE, 
+               KC_TAB,            KC_Q, KC_W, KC_E, KC_R, KC_T,                 LT(1,      KC_NO), 
+               LCTL(KC_RBRACKET), KC_A, KC_S, KC_D, KC_F, MT(MOD_LCTL|MOD_LGUI, KC_G),     
+               KC_LSHIFT,         KC_Z, KC_X, KC_C, KC_V, KC_B,                 LT(2,      KC_NO), 
 
-               KC_LCTL,        KC_TRNS,   KC_TRNS,   KC_LALT,   KC_LSHIFT,
+               KC_LCTL,        KC_TRNS,   KC_TRNS,   KC_LALT,   KC_LGUI,
 
                KC_LEFT,        KC_RIGHT,
                KC_PGUP,
-               KC_LGUI,        KC_LCTL,  KC_PGDOWN,
+               KC_LSHIFT,        KC_LCTL,  KC_PGDOWN,
 
                /* 右手 */
                LCTL(KC_RBRACKET),      KC_6,     KC_7,      KC_8,      KC_9,      KC_0,     KC_BSPACE,
