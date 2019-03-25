6e0002450e2a46839cef0c130a21bc77f0817d7a
shuji ochi <ponpoko1968@gmail.com>
2019-03-25 20:15:14 +0900

    raiseレイヤーにPgUpPgDnを追加

--- keyboards/helix/rev2/keymaps/ponpoko1968/keymap.c
+++ keyboards/helix/rev2/keymaps/ponpoko1968/keymap.c
@@ -157,8 +157,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT( \
                     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_F11, \
                     _______,  _______, KC_7 ,   KC_8,   KC_9,  KC_TILDE,                    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLASH, KC_F12, \
-                    _______, _______, KC_4 ,   KC_5,    KC_6,  _______,                     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,   _______, \
-                    _______, _______, KC_1 ,   KC_2,    KC_4,  KC_GRAVE,  KC_PGUP, _______,  KC_QUOT, KC_DQT,  _______, _______, _______,    _______, \
+                    _______, KC_HOME ,KC_4 ,   KC_5,    KC_6,  _______,                     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,   _______, \
+                    _______, KC_END,  KC_1 ,   KC_2,    KC_4,  KC_GRAVE,  KC_PGUP, _______,  KC_QUOT, KC_DQT,  _______, _______, _______,    _______, \
                     _______, _______, _______, _______, KC_0,   _______,  KC_PGDN, _______,  _______, _______, _______, _______, _______,    _______ \
       ),
 
