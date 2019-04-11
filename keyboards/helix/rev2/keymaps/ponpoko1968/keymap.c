c92bd563235c7c3169a68a6a131f647ede102601
shuji ochi <ponpoko1968@gmail.com>
2019-04-11 20:53:02 +0900

    親指モードでもCtrlキーは効くようにした

--- keyboards/helix/rev2/keymaps/ponpoko1968/keymap.c
+++ keyboards/helix/rev2/keymaps/ponpoko1968/keymap.c
@@ -51,11 +51,15 @@ bool isOyayubiPressed   = false;
 bool isLeftShiftPressed = false;
 bool isRightShiftPressed = false;
 
+bool isLeftCtrlPressed = false;
+
+
 typedef struct {
   uint8_t macro_id;
   uint16_t base[3];
   uint16_t lshift[3];
   uint16_t rshift[3];
+  uint16_t lctl[3];
 } macro_to_sequence_t;
 
 
@@ -81,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_TAB             ,  KC_Q   ,    KC_W  ,    KC_E ,    KC_R  ,    KC_T   ,   /* dummy , dummy          , */  KC_Y ,    KC_U   ,    KC_I    ,    KC_O ,    KC_P    , KC_BSPC  , \
                      LCTL(KC_RBRACKET)  ,  KC_A   ,    KC_S  ,    KC_D ,    KC_F,      KC_G ,   /* dummy , dummy          , */    KC_H ,    KC_J   ,    KC_K    ,    KC_L ,LCTL_T(KC_SCLN) , KC_ENT  , \
                      KC_LSFT            ,  KC_Z   ,    KC_X  ,    KC_C ,    KC_V  ,    KC_B   ,     KC_INS  ,KC_F1,               KC_N ,    KC_M   ,    KC_COMM , KC_DOT  ,    KC_SLSH , KC_RSFT , \
-                     KC_LCTL            ,  ADJUST ,    LOWER ,    RAISE,  KC_LALT , KC_LGUI,     LOWER  , LGUI(KC_SPACE),     LT(_RAISE,KC_SPACE),      OYAYUBI ,    KC_LEFT , KC_DOWN ,    KC_UP   , KC_RGHT \
+                    KC_LCTL,               ADJUST ,    LOWER ,    RAISE,  KC_LALT , KC_LGUI,     LOWER  , LGUI(KC_SPACE),     LT(_RAISE,KC_SPACE),      OYAYUBI ,    KC_LEFT , KC_DOWN ,    KC_UP   , KC_RGHT \
                       ),
 
   /* Lower
@@ -171,11 +175,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_OYA] =  LAYOUT( \
-                   _______ ,   M(0)    ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                   _______ ,   _______ ,    M(11)  ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                   _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                   _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,   _______ ,   _______ ,       _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                   _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,M(100)       , M(101)        , _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ \
+                   _______ ,    M(0)   ,     M(1),      M(2),      M(3),        M(4),                                 M(5)   ,    M(6),       M(7),       M(8),      M(9),      _______, \
+                   _______ ,   M(10)   ,    M(11),     M(12),     M(13),       M(14),                                M(15)   ,   M(16),      M(17),      M(18),     M(19),      _______ , \
+                   _______ ,   M(20)   ,    M(21),     M(22),     M(23),       M(24),                                M(25)   ,   M(26),      M(27),      M(28),     M(29),      _______ , \
+                   _______ ,   M(30)   ,    M(31),     M(32),     M(33),       M(34),  _______ ,   _______,          M(35)   ,   M(36),      M(37),      M(38),     M(39),      _______ , \
+                   M(102)  ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,   M(100),    M(101),          _______ , _______ ,   _______ ,   _______ ,  _______ ,   _______ \
                        ),
   /* 親指シフト（右）
    * ,-----------------------------------------.             ,-----------------------------------------.
@@ -220,46 +224,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 const macro_to_sequence_t macro_to_sequence[] = {
-/*M(0):  1 ? ?       */ {.base={KC_1}, .lshift={KC_NO, 1}, .rshift={KC_NO, 2}},
-/*M(1):  2 ? ?       */ {.base={KC_2}, .lshift={KC_NO, 3}, .rshift={KC_NO, 4}},
-/*M(2):  3 ? ?       */ {.base={KC_3}, .lshift={KC_NO, 5}, .rshift={KC_NO, 6}},
-/*M(3):  4 ? ?       */ {.base={KC_4}, .lshift={KC_NO, 7}, .rshift={KC_NO, 8}},
-/*M(4):  5 ? ?       */ {.base={KC_5}, .lshift={KC_NO, 9}, .rshift={KC_NO, 10}},
-/*M(5):  6 ? ?       */ {.base={KC_6}, .lshift={KC_NO, 11}, .rshift={KC_NO, 12}},
-/*M(6):  7 ? ?       */ {.base={KC_7}, .lshift={KC_NO, 13}, .rshift={KC_NO, 14}},
-/*M(7):  8 ? ?       */ {.base={KC_8}, .lshift={KC_NO, 15}, .rshift={KC_NO, 16}},
-/*M(8):  9 ? ?       */ {.base={KC_9}, .lshift={KC_NO, 17}, .rshift={KC_NO, 18}},
-/*M(9):  0 ? ?       */ {.base={KC_0}, .lshift={KC_NO, 19}, .rshift={KC_NO, 20}},
-/*M(10): ? xa ?      */ {.base={KC_NO, 21}, .lshift={KC_X, KC_A}, .rshift={KC_NO, 22}},
-/*M(11): ka e ga     */ {.base={KC_K, KC_A}, .lshift={KC_E}, .rshift={KC_G, KC_A}},
-/*M(12): ta ri da    */ {.base={KC_T, KC_A}, .lshift={KC_R, KC_I}, .rshift={KC_D, KC_A}},
-/*M(13): ko xya go   */ {.base={KC_K, KC_O}, .lshift={KC_X, KC_Y, KC_A}, .rshift={KC_G, KC_O}},
-/*M(14): sa re za    */ {.base={KC_S, KC_A}, .lshift={KC_R, KC_E}, .rshift={KC_Z, KC_A}},
-/*M(15): ra pa yo    */ {.base={KC_R, KC_A}, .lshift={KC_P, KC_A}, .rshift={KC_Y, KC_O}},
-/*M(16): ti di ni    */ {.base={KC_T, KC_I}, .lshift={KC_D, KC_I}, .rshift={KC_N, KC_I}},
-/*M(17): ku gu ru    */ {.base={KC_K, KC_U}, .lshift={KC_G, KC_U}, .rshift={KC_R, KC_U}},
-/*M(18): tu du ma    */ {.base={KC_T, KC_U}, .lshift={KC_D, KC_U}, .rshift={KC_M, KC_A}},
-/*M(19): ? pi xe     */ {.base={KC_NO, 23}, .lshift={KC_P, KC_I}, .rshift={KC_X, KC_E}},
-/*M(20): u wo vu     */ {.base={KC_U}, .lshift={KC_W, KC_O}, .rshift={KC_V, KC_U}},
-/*M(21): si a ji     */ {.base={KC_S, KC_I}, .lshift={KC_A}, .rshift={KC_J, KC_I}},
-/*M(22): te na de    */ {.base={KC_T, KC_E}, .lshift={KC_N, KC_A}, .rshift={KC_D, KC_E}},
-/*M(23): ke xyu ge   */ {.base={KC_K, KC_E}, .lshift={KC_X, KC_Y, KC_U}, .rshift={KC_G, KC_E}},
-/*M(24): se mo ze    */ {.base={KC_S, KC_E}, .lshift={KC_M, KC_O}, .rshift={KC_Z, KC_E}},
-/*M(25): ha ba mi    */ {.base={KC_H, KC_A}, .lshift={KC_B, KC_A}, .rshift={KC_M, KC_I}},
-/*M(26): to do o     */ {.base={KC_T, KC_O}, .lshift={KC_D, KC_O}, .rshift={KC_O}},
-/*M(27): ki gi no    */ {.base={KC_K, KC_I}, .lshift={KC_G, KC_I}, .rshift={KC_N, KC_O}},
-/*M(28): i po xyo    */ {.base={KC_I}, .lshift={KC_P, KC_O}, .rshift={KC_X, KC_Y, KC_O}},
-/*M(29): nn ? xtu    */ {.base={KC_N, KC_N}, .lshift={KC_NO, 24}, .rshift={KC_X, KC_T, KC_U}},
-/*M(30): ? xu ?     */ {.base={KC_NO, 25}, .lshift={KC_X, KC_U}, .rshift={KC_NO, 26}},
-/*M(31): hi - bi     */ {.base={KC_H, KC_I}, .lshift={KC_MINUS}, .rshift={KC_B, KC_I}},
-/*M(32): su ro zu    */ {.base={KC_S, KC_U}, .lshift={KC_R, KC_O}, .rshift={KC_Z, KC_U}},
-/*M(33): fu ya bu    */ {.base={KC_F, KC_U}, .lshift={KC_Y, KC_A}, .rshift={KC_B, KC_U}},
-/*M(34): he xi be    */ {.base={KC_H, KC_E}, .lshift={KC_X, KC_I}, .rshift={KC_B, KC_E}},
-/*M(35): me pu nu    */ {.base={KC_M, KC_E}, .lshift={KC_P, KC_U}, .rshift={KC_N, KC_U}},
-/*M(36): so zo yu    */ {.base={KC_S, KC_O}, .lshift={KC_Z, KC_O}, .rshift={KC_Y, KC_U}},
-/*M(37): ne pe mu    */ {.base={KC_N, KC_E}, .lshift={KC_P, KC_E}, .rshift={KC_M, KC_U}},
-/*M(38): ho bo wa    */ {.base={KC_H, KC_O}, .lshift={KC_B, KC_O}, .rshift={KC_W, KC_A}},
-/*M(39): ? ? xo      */ {.base={KC_NO, 27}, .lshift={KC_NO, 28}, .rshift={KC_X, KC_O}},
+  /*M(0):  1 ? ?       */ {.base={KC_1}       , .lshift={KC_NO, 1}         , .rshift={KC_NO, 2}         ,  .lctl={KC_NO,0}},
+  /*M(1):  2 ? ?       */ {.base={KC_2}       , .lshift={KC_NO, 3}         , .rshift={KC_NO, 4}         ,  .lctl={KC_NO,0}},
+  /*M(2):  3 ? ?       */ {.base={KC_3}       , .lshift={KC_NO, 5}         , .rshift={KC_NO, 6}         ,  .lctl={KC_NO,0}},
+  /*M(3):  4 ? ?       */ {.base={KC_4}       , .lshift={KC_NO, 7}         , .rshift={KC_NO, 8}         ,  .lctl={KC_NO,0}},
+  /*M(4):  5 ? ?       */ {.base={KC_5}       , .lshift={KC_NO, 9}         , .rshift={KC_NO, 10}        ,  .lctl={KC_NO,0}},
+  /*M(5):  6 ? ?       */ {.base={KC_6}       , .lshift={KC_NO, 11}        , .rshift={KC_NO, 12}        ,  .lctl={KC_NO,0}},
+  /*M(6):  7 ? ?       */ {.base={KC_7}       , .lshift={KC_NO, 13}        , .rshift={KC_NO, 14}        ,  .lctl={KC_NO,0}},
+  /*M(7):  8 ? ?       */ {.base={KC_8}       , .lshift={KC_NO, 15}        , .rshift={KC_NO, 16}        ,  .lctl={KC_NO,0}},
+  /*M(8):  9 ? ?       */ {.base={KC_9}       , .lshift={KC_NO, 17}        , .rshift={KC_NO, 18}        ,  .lctl={KC_NO,0}},
+  /*M(9):  0 ? ?       */ {.base={KC_0}       , .lshift={KC_NO, 19}        , .rshift={KC_NO, 20}        ,  .lctl={KC_NO,0}},
+
+  /*M(10): ? xa ?      */ {.base={KC_NO, 21}  , .lshift={KC_X, KC_A}       , .rshift={KC_NO, 22}        ,  .lctl={KC_Q}},
+  /*M(11): ka e ga     */ {.base={KC_K, KC_A} , .lshift={KC_E}             , .rshift={KC_G, KC_A}       ,  .lctl={KC_W}},
+  /*M(12): ta ri da    */ {.base={KC_T, KC_A} , .lshift={KC_R, KC_I}       , .rshift={KC_D, KC_A}       ,  .lctl={KC_E}},
+  /*M(13): ko xya go   */ {.base={KC_K, KC_O} , .lshift={KC_X, KC_Y, KC_A} , .rshift={KC_G, KC_O}       ,  .lctl={KC_R}},
+  /*M(14): sa re za    */ {.base={KC_S, KC_A} , .lshift={KC_R, KC_E}       , .rshift={KC_Z, KC_A}       ,  .lctl={KC_T}},
+  /*M(15): ra pa yo    */ {.base={KC_R, KC_A} , .lshift={KC_P, KC_A}       , .rshift={KC_Y, KC_O}       ,  .lctl={KC_Y}},
+  /*M(16): ti di ni    */ {.base={KC_T, KC_I} , .lshift={KC_D, KC_I}       , .rshift={KC_N, KC_I}       ,  .lctl={KC_U}},
+  /*M(17): ku gu ru    */ {.base={KC_K, KC_U} , .lshift={KC_G, KC_U}       , .rshift={KC_R, KC_U}       ,  .lctl={KC_I}},
+  /*M(18): tu du ma    */ {.base={KC_T, KC_U} , .lshift={KC_D, KC_U}       , .rshift={KC_M, KC_A}       ,  .lctl={KC_O}},
+  /*M(19): ? pi xe     */ {.base={KC_NO, 23}  , .lshift={KC_P, KC_I}       , .rshift={KC_X, KC_E}       ,  .lctl={KC_P}},
+
+  /*M(20): u wo vu     */ {.base={KC_U}       , .lshift={KC_W, KC_O}       , .rshift={KC_V, KC_U}       ,  .lctl={KC_A}},
+  /*M(21): si a ji     */ {.base={KC_S, KC_I} , .lshift={KC_A}             , .rshift={KC_J, KC_I}       ,  .lctl={KC_S}},
+  /*M(22): te na de    */ {.base={KC_T, KC_E} , .lshift={KC_N, KC_A}       , .rshift={KC_D, KC_E}       ,  .lctl={KC_D}},
+  /*M(23): ke xyu ge   */ {.base={KC_K, KC_E} , .lshift={KC_X, KC_Y, KC_U} , .rshift={KC_G, KC_E}       ,  .lctl={KC_F}},
+  /*M(24): se mo ze    */ {.base={KC_S, KC_E} , .lshift={KC_M, KC_O}       , .rshift={KC_Z, KC_E}       ,  .lctl={KC_G}},
+  /*M(25): ha ba mi    */ {.base={KC_H, KC_A} , .lshift={KC_B, KC_A}       , .rshift={KC_M, KC_I}       ,  .lctl={KC_H}},
+  /*M(26): to do o     */ {.base={KC_T, KC_O} , .lshift={KC_D, KC_O}       , .rshift={KC_O}             ,  .lctl={KC_J}},
+  /*M(27): ki gi no    */ {.base={KC_K, KC_I} , .lshift={KC_G, KC_I}       , .rshift={KC_N, KC_O}       ,  .lctl={KC_K}},
+  /*M(28): i po xyo    */ {.base={KC_I}       , .lshift={KC_P, KC_O}       , .rshift={KC_X, KC_Y, KC_O} ,  .lctl={KC_L}},
+  /*M(29): nn ? xtu    */ {.base={KC_N, KC_N} , .lshift={KC_NO, 24}        , .rshift={KC_X, KC_T, KC_U} ,  .lctl={KC_NO}},
+
+  /*M(30): ? xu ?     */ {.base={KC_NO, 25}   , .lshift={KC_X, KC_U}       , .rshift={KC_NO, 26}        ,  .lctl={KC_Z}},
+  /*M(31): hi - bi     */ {.base={KC_H, KC_I} , .lshift={KC_MINUS}         , .rshift={KC_B, KC_I}       ,  .lctl={KC_X}},
+  /*M(32): su ro zu    */ {.base={KC_S, KC_U} , .lshift={KC_R, KC_O}       , .rshift={KC_Z, KC_U}       ,  .lctl={KC_C}},
+  /*M(33): fu ya bu    */ {.base={KC_F, KC_U} , .lshift={KC_Y, KC_A}       , .rshift={KC_B, KC_U}       ,  .lctl={KC_V}},
+  /*M(34): he xi be    */ {.base={KC_H, KC_E} , .lshift={KC_X, KC_I}       , .rshift={KC_B, KC_E}       ,  .lctl={KC_B}},
+  /*M(35): me pu nu    */ {.base={KC_M, KC_E} , .lshift={KC_P, KC_U}       , .rshift={KC_N, KC_U}       ,  .lctl={KC_N}},
+  /*M(36): so zo yu    */ {.base={KC_S, KC_O} , .lshift={KC_Z, KC_O}       , .rshift={KC_Y, KC_U}       ,  .lctl={KC_M}},
+  /*M(37): ne pe mu    */ {.base={KC_N, KC_E} , .lshift={KC_P, KC_E}       , .rshift={KC_M, KC_U}       ,  .lctl={KC_NO,0}},
+  /*M(38): ho bo wa    */ {.base={KC_H, KC_O} , .lshift={KC_B, KC_O}       , .rshift={KC_W, KC_A}       ,  .lctl={KC_NO,0}},
+  /*M(39): ? ? xo      */ {.base={KC_NO, 27}  , .lshift={KC_NO, 28}        , .rshift={KC_X, KC_O}       ,  .lctl={KC_NO,0}},
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -271,6 +278,8 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         seq = macro_to_sequence[id].lshift;
       }else if(isRightShiftPressed){
         seq = macro_to_sequence[id].rshift;
+      }else if(isLeftCtrlPressed){
+        seq = macro_to_sequence[id].lctl;
       }else{
         seq = macro_to_sequence[id].base;
       }
@@ -373,8 +382,15 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         if (seq[i] == KC_NO) {
           break;
         }
-        register_code(seq[i]);
-        unregister_code(seq[i]);
+        if(isLeftCtrlPressed){
+          register_code(KC_LCTRL);
+          register_code(seq[i]);
+          unregister_code(seq[i]);
+          unregister_code(KC_LCTRL);
+        }else{
+          register_code(seq[i]);
+          unregister_code(seq[i]);
+        }
       }
     }
     return MACRO_NONE;
@@ -394,6 +410,14 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       isRightShiftPressed = true;
     }else{
       isRightShiftPressed = false;
+    }
+    break;
+
+  case 102:
+    if(record->event.pressed){
+      isLeftCtrlPressed = true;
+    }else{
+      isLeftCtrlPressed = false;
     }
     break;
   }
