33fedfd5353f61e64d21eb1fdcd9f3a199ad39cc
shuji ochi <ponpoko1968@gmail.com>
2019-04-11 18:31:53 +0900

    西尾泰和氏のマクロを適用

--- keyboards/helix/rev2/keymaps/ponpoko1968/keymap.c
+++ keyboards/helix/rev2/keymaps/ponpoko1968/keymap.c
@@ -29,9 +29,7 @@ enum layer_number {
     _LOWER,                     /* 1 */
     _RAISE,                     /* 2 */
     _OYA,                       /* 3 */
-    _OYA_RIGHT,                 /* 4 */
-    _OYA_LEFT,                  /* 5 */
-    _ADJUST                     /* 6 */
+    _ADJUST                     /* 4*/
 };
 
 enum custom_keycodes {
@@ -42,18 +40,21 @@ enum custom_keycodes {
   BACKLIT,
   EISU,
   KANA,
-  RGBRST,
-  OYA,
-  OYA_RIGHT,
-  OYA_LEFT,
+  RGBRST
 };
 
-enum macro_keycodes {
-  KC_SAMPLEMACRO,
-};
+ /* https://github.com/nishio/qmk_firmware/blob/thumb_shift/keyboards/ergodox/keymaps/thumb_shift/keymap.c */
+bool isLeftShiftPressed = false;
+bool isRightShiftPressed = false;
 
-//Macros
-#define M_SAMPLE M(KC_SAMPLEMACRO)
+typedef struct {
+  uint8_t macro_id;
+  uint16_t base[3];
+  uint16_t lshift[3];
+  uint16_t rshift[3];
+} macro_to_sequence_t;
+
+
 
 #if HELIX_ROWS == 5
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -121,6 +122,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     _______ , _______  , TO(_QWERTY) , TO(_QWERTY) , KC_0    ,   _______ ,  KC_PGDN,_______,  _______ , _______ , _______   , _______   , _______ ,    _______ \
       ),
 
+  /*
+
+    qmk_firmware/keymap.c at thumb_shift · nishio/qmk_firmware
+    https://github.com/nishio/qmk_firmware/blob/thumb_shift/keyboards/ergodox/keymaps/thumb_shift/keymap.c
+
+    [BASE] = KEYMAP(
+    // layer 0: Thumb Shift
+    // left hand
+    KC_ESC,  M(0),  M(1) , M(2),  M(3),  M(4),  KC_NO,
+    KC_TAB,  M(10), M(11), M(12), M(13), M(14), KC_UP,
+    KC_TAB,  M(20), M(21), M(22), M(23), M(24),
+    KC_LSFT, M(30), M(31), M(32), M(33), M(34), KC_DOWN,
+    TO(1),   KC_NO, KC_NO, KC_NO, KC_NO,
+
+    /*   */ KC_NO,    KC_NO,
+  /*           */   KC_NO,
+  M(100), KC_SPACE, KC_NO,
+
+  // right hand
+  KC_NO,   M(5),  M(6),    M(7),     M(8),  M(9),  KC_BSPACE,
+  KC_NO,   M(15), M(16),   M(17),    M(18), M(19), KC_NO,
+  /*    */ M(25), M(26),   M(27),    M(28), M(29), KC_NO,
+  KC_NO,   M(35), M(36),   M(37),    M(38), M(39), KC_NO,
+  /*           */ KC_LEFT, KC_RIGHT, KC_NO, KC_NO, KC_NO,
+
+  KC_NO, KC_NO,
+  KC_NO,
+  KC_NO, KC_ENTER, M(101)
+  ),
+   */
+
   /* 親指シフト（ベース）
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |      |      |      |      |      |      |             |      |      |      |      |      |      |
@@ -135,11 +167,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_OYA] =  LAYOUT( \
-                   _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                   _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
+                   _______ ,   M(0)    ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
+                   _______ ,   _______ ,    M(11)  ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
                    _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
                    _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,   _______ ,   _______ ,       _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                   _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,MO(_OYA_LEFT), MO(_OYA_RIGHT), _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______  \
+                   _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,M(100)       , M(101)        , _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ \
                        ),
   /* 親指シフト（右）
    * ,-----------------------------------------.             ,-----------------------------------------.
@@ -154,34 +186,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
-  [_OYA_RIGHT] =  LAYOUT( \
-                         _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                         _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                         _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                         _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,   _______ ,   _______ ,       _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                         _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,   _______ ,   _______ ,       _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______  \
-                       ),
-  /* 親指シフト（左）
-   * ,-----------------------------------------.             ,-----------------------------------------.
-   * |      |      |      |      |      |      |             |      |      |      |      |      |      |
-   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |             |      |      |      |      |      |      |
-   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |             |      |      |      |      |      |      |
-   * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
-   * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
-   * `-------------------------------------------------------------------------------------------------'
-   */
-  [_OYA_LEFT] =  LAYOUT( \
-                        _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                        _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                        _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,                               _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                        _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,   _______ ,   _______ ,       _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______ , \
-                        _______ ,   _______ ,   _______ ,   _______ , _______ ,   _______ ,   _______ ,   _______ ,       _______ ,   _______ ,   _______ ,   _______ ,  _______ ,  _______  \
-                       ),
-
 
   /* Adjust (Lower + Raise)
    * ,-----------------------------------------.             ,-----------------------------------------.
@@ -208,6 +212,190 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #else
 #error "undefined keymaps"
 #endif
+
+
+
+const macro_to_sequence_t macro_to_sequence[] = {
+/*M(0):  1 ? ?       */ {.base={KC_1}, .lshift={KC_NO, 1}, .rshift={KC_NO, 2}},
+/*M(1):  2 ? ?       */ {.base={KC_2}, .lshift={KC_NO, 3}, .rshift={KC_NO, 4}},
+/*M(2):  3 ? ?       */ {.base={KC_3}, .lshift={KC_NO, 5}, .rshift={KC_NO, 6}},
+/*M(3):  4 ? ?       */ {.base={KC_4}, .lshift={KC_NO, 7}, .rshift={KC_NO, 8}},
+/*M(4):  5 ? ?       */ {.base={KC_5}, .lshift={KC_NO, 9}, .rshift={KC_NO, 10}},
+/*M(5):  6 ? ?       */ {.base={KC_6}, .lshift={KC_NO, 11}, .rshift={KC_NO, 12}},
+/*M(6):  7 ? ?       */ {.base={KC_7}, .lshift={KC_NO, 13}, .rshift={KC_NO, 14}},
+/*M(7):  8 ? ?       */ {.base={KC_8}, .lshift={KC_NO, 15}, .rshift={KC_NO, 16}},
+/*M(8):  9 ? ?       */ {.base={KC_9}, .lshift={KC_NO, 17}, .rshift={KC_NO, 18}},
+/*M(9):  0 ? ?       */ {.base={KC_0}, .lshift={KC_NO, 19}, .rshift={KC_NO, 20}},
+/*M(10): ? xa ?      */ {.base={KC_NO, 21}, .lshift={KC_X, KC_A}, .rshift={KC_NO, 22}},
+/*M(11): ka e ga     */ {.base={KC_K, KC_A}, .lshift={KC_E}, .rshift={KC_G, KC_A}},
+/*M(12): ta ri da    */ {.base={KC_T, KC_A}, .lshift={KC_R, KC_I}, .rshift={KC_D, KC_A}},
+/*M(13): ko xya go   */ {.base={KC_K, KC_O}, .lshift={KC_X, KC_Y, KC_A}, .rshift={KC_G, KC_O}},
+/*M(14): sa re za    */ {.base={KC_S, KC_A}, .lshift={KC_R, KC_E}, .rshift={KC_Z, KC_A}},
+/*M(15): ra pa yo    */ {.base={KC_R, KC_A}, .lshift={KC_P, KC_A}, .rshift={KC_Y, KC_O}},
+/*M(16): ti di ni    */ {.base={KC_T, KC_I}, .lshift={KC_D, KC_I}, .rshift={KC_N, KC_I}},
+/*M(17): ku gu ru    */ {.base={KC_K, KC_U}, .lshift={KC_G, KC_U}, .rshift={KC_R, KC_U}},
+/*M(18): tu du ma    */ {.base={KC_T, KC_U}, .lshift={KC_D, KC_U}, .rshift={KC_M, KC_A}},
+/*M(19): ? pi xe     */ {.base={KC_NO, 23}, .lshift={KC_P, KC_I}, .rshift={KC_X, KC_E}},
+/*M(20): u wo vu     */ {.base={KC_U}, .lshift={KC_W, KC_O}, .rshift={KC_V, KC_U}},
+/*M(21): si a ji     */ {.base={KC_S, KC_I}, .lshift={KC_A}, .rshift={KC_J, KC_I}},
+/*M(22): te na de    */ {.base={KC_T, KC_E}, .lshift={KC_N, KC_A}, .rshift={KC_D, KC_E}},
+/*M(23): ke xyu ge   */ {.base={KC_K, KC_E}, .lshift={KC_X, KC_Y, KC_U}, .rshift={KC_G, KC_E}},
+/*M(24): se mo ze    */ {.base={KC_S, KC_E}, .lshift={KC_M, KC_O}, .rshift={KC_Z, KC_E}},
+/*M(25): ha ba mi    */ {.base={KC_H, KC_A}, .lshift={KC_B, KC_A}, .rshift={KC_M, KC_I}},
+/*M(26): to do o     */ {.base={KC_T, KC_O}, .lshift={KC_D, KC_O}, .rshift={KC_O}},
+/*M(27): ki gi no    */ {.base={KC_K, KC_I}, .lshift={KC_G, KC_I}, .rshift={KC_N, KC_O}},
+/*M(28): i po xyo    */ {.base={KC_I}, .lshift={KC_P, KC_O}, .rshift={KC_X, KC_Y, KC_O}},
+/*M(29): nn ? xtu    */ {.base={KC_N, KC_N}, .lshift={KC_NO, 24}, .rshift={KC_X, KC_T, KC_U}},
+/*M(30): ? xu ?     */ {.base={KC_NO, 25}, .lshift={KC_X, KC_U}, .rshift={KC_NO, 26}},
+/*M(31): hi - bi     */ {.base={KC_H, KC_I}, .lshift={KC_MINUS}, .rshift={KC_B, KC_I}},
+/*M(32): su ro zu    */ {.base={KC_S, KC_U}, .lshift={KC_R, KC_O}, .rshift={KC_Z, KC_U}},
+/*M(33): fu ya bu    */ {.base={KC_F, KC_U}, .lshift={KC_Y, KC_A}, .rshift={KC_B, KC_U}},
+/*M(34): he xi be    */ {.base={KC_H, KC_E}, .lshift={KC_X, KC_I}, .rshift={KC_B, KC_E}},
+/*M(35): me pu nu    */ {.base={KC_M, KC_E}, .lshift={KC_P, KC_U}, .rshift={KC_N, KC_U}},
+/*M(36): so zo yu    */ {.base={KC_S, KC_O}, .lshift={KC_Z, KC_O}, .rshift={KC_Y, KC_U}},
+/*M(37): ne pe mu    */ {.base={KC_N, KC_E}, .lshift={KC_P, KC_E}, .rshift={KC_M, KC_U}},
+/*M(38): ho bo wa    */ {.base={KC_H, KC_O}, .lshift={KC_B, KC_O}, .rshift={KC_W, KC_A}},
+/*M(39): ? ? xo      */ {.base={KC_NO, 27}, .lshift={KC_NO, 28}, .rshift={KC_X, KC_O}},
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  if(id < 40){
+    if (record->event.pressed) {
+      const uint16_t* seq;
+      if(isLeftShiftPressed){
+        seq = macro_to_sequence[id].lshift;
+      }else if(isRightShiftPressed){
+        seq = macro_to_sequence[id].rshift;
+      }else{
+        seq = macro_to_sequence[id].base;
+      }
+      // special commands
+      if(seq[0] == KC_NO && seq[1] > 0){
+        switch(seq[1]){
+        case 1: // "？?"
+          register_code(KC_LSHIFT);
+          register_code(KC_SLASH);
+          unregister_code(KC_SLASH);
+          unregister_code(KC_LSHIFT);
+          break;
+        case 21: // "・/"
+          register_code(KC_SLASH);
+          unregister_code(KC_SLASH);
+          break;
+        case 23: // "、,"
+          register_code(KC_COMMA);
+          unregister_code(KC_COMMA);
+          break;
+        case 27: // "。."
+          register_code(KC_DOT);
+          unregister_code(KC_DOT);
+          break;
+        case 7: // "「["
+          register_code(KC_LBRACKET);
+          unregister_code(KC_LBRACKET);
+          register_code(KC_ENTER);
+          unregister_code(KC_ENTER);
+          break;
+        case 8: // "『"
+          register_code(KC_LBRACKET);
+          unregister_code(KC_LBRACKET);
+          register_code(KC_SPACE);
+          unregister_code(KC_SPACE);
+          register_code(KC_ENTER);
+          unregister_code(KC_ENTER);
+          break;
+        case 9: // "」]"
+          register_code(KC_RBRACKET);
+          unregister_code(KC_RBRACKET);
+          register_code(KC_ENTER);
+          unregister_code(KC_ENTER);
+          break;
+        case 10: // "』"
+          register_code(KC_RBRACKET);
+          unregister_code(KC_RBRACKET);
+          register_code(KC_SPACE);
+          unregister_code(KC_SPACE);
+          register_code(KC_ENTER);
+          unregister_code(KC_ENTER);
+          break;
+        case 11: // "["
+          register_code(KC_LBRACKET);
+          unregister_code(KC_LBRACKET);
+          register_code(KC_F10);
+          unregister_code(KC_F10);
+          break;
+        case 12: // "("
+          register_code(KC_LSHIFT);
+          register_code(KC_9);
+          unregister_code(KC_9);
+          unregister_code(KC_LSHIFT);
+          break;
+        case 13: // "]"
+          register_code(KC_RBRACKET);
+          unregister_code(KC_RBRACKET);
+          register_code(KC_F10);
+          unregister_code(KC_F10);
+          break;
+        case 14: // ")"
+          register_code(KC_LSHIFT);
+          register_code(KC_0);
+          unregister_code(KC_0);
+          unregister_code(KC_LSHIFT);
+          break;
+        case 20: // F10
+          register_code(KC_F10);
+          unregister_code(KC_F10);
+          break;
+        case 25: // Undo: Ctrl-Z
+          register_code(KC_LCTRL);
+          register_code(KC_Z);
+          unregister_code(KC_Z);
+          unregister_code(KC_LCTRL);
+          break;
+        case 26: // Redo: Ctrl-Y
+          register_code(KC_LCTRL);
+          register_code(KC_Y);
+          unregister_code(KC_Y);
+          unregister_code(KC_LCTRL);
+          break;
+        }
+        return MACRO_NONE;
+      }
+      // finish special commands
+
+      // send normal sequence
+      for (int i = 0; i < 3; i++) {
+        if (seq[i] == KC_NO) {
+          break;
+        }
+        register_code(seq[i]);
+        unregister_code(seq[i]);
+      }
+    }
+    return MACRO_NONE;
+  }
+
+  // update thumb shift status
+  switch(id) {
+  case 100:
+    if(record->event.pressed){
+      isLeftShiftPressed = true;
+    }else{
+      isLeftShiftPressed = false;
+    }
+    break;
+  case 101:
+    if(record->event.pressed){
+      isRightShiftPressed = true;
+    }else{
+      isRightShiftPressed = false;
+    }
+    break;
+  }
+
+  return MACRO_NONE;
+};
 
 
 #ifdef AUDIO_ENABLE
@@ -245,9 +433,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
   case QWERTY:
     if (record->event.pressed) {
-#ifdef AUDIO_ENABLE
-      PLAY_SONG(tone_qwerty);
-#endif
       persistent_default_layer_set(1UL<<_QWERTY);
     }
     return false;
@@ -259,16 +444,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (TOG_STATUS) { //TOG_STATUS checks is another reactive key currently pressed, only changes RGB mode if returns false
       } else {
         TOG_STATUS = !TOG_STATUS;
-#ifdef RGBLIGHT_ENABLE
-        //rgblight_mode(RGBLIGHT_MODE_SNAKE + 1);
-#endif
       }
       layer_on(_LOWER);
       update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
     } else {
-#ifdef RGBLIGHT_ENABLE
-      //rgblight_mode(RGB_current_mode);   // revert RGB to initial mode prior to RGB mode change
-#endif
       TOG_STATUS = false;
       layer_off(_LOWER);
       update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
@@ -301,30 +480,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return false;
     break;
 
-   case OYA:
-    if (record->event.pressed) {
-      persistent_default_layer_set(1UL<<_OYA);
-    }
-    return false;
-    break;
-
-  case OYA_RIGHT:
-    if (record->event.pressed) {
-      layer_on(_OYA_RIGHT);
-    } else {
-      layer_off(_OYA_RIGHT);
-    }
-    return false;
-    break;
-
-  case OYA_LEFT:
-    if (record->event.pressed) {
-      layer_on(_OYA_LEFT);
-    } else {
-      layer_off(_OYA_LEFT);
-    }
-    return false;
-    break;
 
   case ADJUST:
     if (record->event.pressed) {
@@ -442,8 +597,6 @@ void matrix_update(struct CharacterMatrix *dest,
 #define L_LOWER (1<<_LOWER)
 #define L_RAISE (1<<_RAISE)
 #define L_OYA (1<<_OYA)
-#define L_OYA_RIGHT ((1<<_OYA_RIGHT) + (1<<_OYA))
-#define L_OYA_LEFT ((1<<_OYA_LEFT) + (1<<_OYA))
 #define L_ADJUST (1<<_ADJUST)
 #define L_ADJUST_TRI (L_ADJUST|L_RAISE|L_LOWER)
 
@@ -494,12 +647,6 @@ void render_status(struct CharacterMatrix *matrix) {
     break;
   case L_OYA:
     matrix_write_P(matrix, PSTR("OYA"));
-    break;
-  case L_OYA_LEFT:
-    matrix_write_P(matrix, PSTR("Left"));
-    break;
-  case L_OYA_RIGHT:
-    matrix_write_P(matrix, PSTR("Right"));
     break;
   default:
     matrix_write(matrix, buf);
