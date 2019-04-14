89dcddfd6abc176e57b7a393b53e6f39b63f2b29
shuji ochi <ponpoko1968@gmail.com>
2019-04-14 16:09:15 +0900

    raise/lowerを元通りに

--- keyboards/helix/rev2/keymaps/ponpoko1968/keymap.c
+++ keyboards/helix/rev2/keymaps/ponpoko1968/keymap.c
@@ -100,41 +100,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
-                     KC_ESC            ,  KC_1   ,    KC_2  ,    KC_3 ,    KC_4  ,    KC_5   ,   /* dummy , dummy          , */  KC_6 ,    KC_7   ,    KC_8    ,    KC_9 ,    KC_0    , KC_DEL , \
-                     KC_TAB            ,  KC_Q   ,    KC_W  ,    KC_E ,    KC_R  ,    KC_T   ,   /* dummy , dummy          , */  KC_Y ,    KC_U   ,    KC_I    ,    KC_O ,    KC_P    , KC_BSPC  , \
-                     LCTL(KC_RBRACKET) ,  KC_A   ,    KC_S  ,    KC_D ,    KC_F  ,    KC_G   ,   /* dummy , dummy          , */  KC_H ,    KC_J   ,    KC_K    ,    KC_L ,    KC_SCLN , KC_ENT  , \
-                     KC_LSFT           ,  KC_Z   ,    KC_X  ,    KC_C ,    KC_V  ,    KC_B   ,   KC_INS  , KC_F1        ,     KC_N ,    KC_M   ,    KC_COMM , KC_DOT  ,    KC_SLSH , KC_RSFT , \
-                     KC_LCTL           ,  ADJUST ,  KC_LALT , KC_LALT ,  KC_LALT ,    KC_LGUI,   LOWER    , LGUI(KC_SPACE) ,     LT(_RAISE,KC_SPACE), HACHIMITSU  ,    KC_LEFT , KC_DOWN ,    KC_UP   , KC_RGHT \
+                     KC_ESC            ,  KC_1   ,    KC_2   ,    KC_3   ,    KC_4  ,    KC_5    ,   /* dummy , dummy          , */  KC_6      ,    KC_7  ,    KC_8    ,    KC_9    ,    KC_0    , KC_DEL   , \
+                     KC_TAB            ,  KC_Q   ,    KC_W   ,    KC_E   ,    KC_R  ,    KC_T    ,   /* dummy , dummy          , */  KC_Y      ,    KC_U  ,    KC_I    ,    KC_O    ,    KC_P    , KC_BSPC  , \
+                     LCTL(KC_RBRACKET) ,  KC_A   ,    KC_S   ,    KC_D   ,    KC_F  ,    KC_G    ,   /* dummy , dummy          , */  KC_H      ,    KC_J  ,    KC_K    ,    KC_L    ,    KC_SCLN , KC_ENT   , \
+                     KC_LSFT           ,  KC_Z   ,    KC_X   ,    KC_C   ,    KC_V  ,    KC_B    ,   KC_INS   , KC_F1          ,     KC_N      ,    KC_M  ,    KC_COMM , KC_DOT     ,    KC_SLSH , KC_RSFT  , \
+                     KC_LCTL           ,  ADJUST ,TG(_LOWER) ,TG(_RAISE) ,  KC_LALT ,    KC_LGUI ,   LOWER    , LGUI(KC_SPACE) , LT(_RAISE,KC_SPACE), HACHIMITSU ,    KC_LEFT , KC_DOWN    ,    KC_UP , KC_RGHT \
                       ),
 
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |      |   F1 |  F2  | f3   |  f4  | f5   |             |   f6 |  f7  |  f8  |  f9  | f10  | f11  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |      |
+   * |      |      |      |      |      |      |             |   /  |   7  |   8  |  9   |  -   | F12  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   _  |   +  |   {  |   }  |  |   |
+   * |      |      |      |      |      |      |             |   *  |   4  |   5  |  6   |  +   |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  (   |   )  |  F12 |      |      | Home | End  |      |
+   * |      |      |      |      |      |      |      |      |      |   1  |   2  |  3   |  =   |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+   * |      |      |      |      |      |      |      |      |      |   0  |      |  .   |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
-                    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_F11, \
-                    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F12, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_F12,  _______, _______, KC_HOME, KC_END,  _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+                    _______ , KC_F1   ,   KC_F2    ,   KC_F3    ,   KC_F4 ,   KC_F5 ,                        KC_F6 ,   KC_F7   ,   KC_F8 ,   KC_F9 ,   KC_F10 ,    KC_F11 , \
+                    _______ , _______ , _______    , _______    , _______ , _______ ,                        KC_MINUS , KC_7   ,   KC_8    , KC_9    , KC_MINUS ,  KC_F12   , \
+                    _______ , _______ , _______    , _______    , _______ , _______ ,                        KC_PLUS , KC_4    ,   KC_5    , KC_6    , KC_PLUS  ,   _______ , \
+                    _______ , _______ , _______    , _______    , _______ , _______ , _______ ,    _______ , KC_SLSH , KC_1    ,   KC_2     ,KC_3      , KC_EQL  ,    _______ , \
+                    _______ , _______ , _______    , _______    , _______ , _______ , _______ ,    _______ , _______ , KC_0    ,   _______,  KC_DOT   , _______ , _______ \
       ),
 
   /* Raise
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | f11  |      |  7   |  8   |  0   |  ~   |             |  -   |   =  |   [  |  ]   |   \  | f12  |
+   * |      |      |  7   |  8   |  0   |  ~   |             |  -   |   =  |   [  |  ]   |   \  | f12  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | f12  |      |  4   |  5   |  6   |      |             |  _   |   -  |   {  | }    |   |  |      |
+   * |      |      |  4   |  5   |  6   |      |             |  _   |   -  |   {  | }    |   |  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |  1   |  2   |  3   |  `   |      |      |  '   |   "  |      |      |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -142,11 +142,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
-                    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-                    _______,  _______, KC_7 ,   KC_8,   KC_9,  KC_TILDE,                    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLASH, KC_F12, \
-                    _______, KC_HOME ,KC_4 ,   KC_5,    KC_6,  _______,                     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,   _______, \
-                    _______, KC_END,  KC_1 ,   KC_2,    KC_4,  KC_GRAVE,  KC_PGUP, _______,  KC_QUOT, KC_DQT,  _______, _______, _______,    _______, \
-                    _______, _______, _______, _______, KC_0,   _______,  KC_PGDN, _______,  _______, _______, _______, _______, _______,    _______ \
+                    KC_TILD , KC_EXLM  , KC_AT       ,   KC_HASH   , KC_DLR  ,  KC_PERC  ,                    KC_CIRC   , KC_AMPR , KC_ASTR  , KC_LPRN , KC_RPRN   , _______   , \
+                    _______ ,  KC_HOME, KC_UP        ,   KC_8      ,   KC_9  ,  KC_TILDE ,                    KC_MINS  ,  KC_EQL  , KC_LBRC ,  KC_RBRC , KC_BSLASH , KC_F12    , \
+                    _______ , KC_LEFT  ,KC_DOWN      ,   KC_RIGHT      ,    KC_6 ,  _______  ,                    KC_UNDS ,   KC_PLUS , KC_LCBR  , KC_RCBR , KC_PIPE   ,   _______ , \
+                    _______ , KC_END   ,  KC_1       ,   KC_2      ,    KC_3 ,  KC_GRAVE ,  KC_PGUP,_______,  KC_QUOT ,   KC_DQT  ,  _______  , _______   , _______ ,    _______ , \
+                    _______ , _______  , TO(_QWERTY) , TO(_QWERTY) , KC_0    ,   _______ ,  KC_PGDN,_______,  _______ , _______ , _______   , _______   , _______ ,    _______ \
       ),
 
   [_JAPANESE] = LAYOUT( \
