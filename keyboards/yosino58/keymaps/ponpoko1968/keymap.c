24567aa554070501e61d285219ee6073b90e51d4
shuji ochi <ponpoko1968@gmail.com>
2019-05-31 14:08:30 +0900

    テンキーパッドを設定

--- keyboards/yosino58/keymaps/ponpoko1968/keymap.c
+++ keyboards/yosino58/keymaps/ponpoko1968/keymap.c
@@ -51,9 +51,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          `-----------------------------'         '------------------------------'
  */
  [_QWERTY] = LAYOUT( \
-                    KC_ESC,   KC_1, KC_2, KC_3, KC_4,    KC_5,                                                 KC_6,    KC_7,    KC_8,    KC_9,   KC_0,  LCTL(KC_RBRACKET), \
+                    KC_GESC,   KC_1, KC_2, KC_3, KC_4,    KC_5,                                                 KC_6,    KC_7,    KC_8,    KC_9,   KC_0,  LCTL(KC_RBRACKET), \
                     KC_TAB,   KC_Q, KC_W, KC_E, KC_R,    KC_T,                                                 KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC, \
-                    LCTL(KC_RBRACKET),  KC_A, KC_S, KC_D, KC_F,    KC_G,                                                 KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT, \
+                    KC_LSFT,  KC_A, KC_S, KC_D, KC_F,    KC_G,                                                 KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RSFT, \
                     KC_LCTL, KC_Z, KC_X, KC_C, KC_V,    KC_B,  KC_LALT, C_M_T(TG(_LOWER)), KC_ENT,LGUI(KC_SPACE), KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RCTRL, \
                     /*                  */ MT(MOD_LSFT,KC_LCBR),   MT(MOD_LCTL,KC_RCBR), KC_LGUI,           LT(_RAISE,KC_SPACE),   KC_LCPO,KC_RSPC \
                      ),
@@ -73,11 +73,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          `-----------------------------'         '------------------------------'
  */
  [_LOWER] = LAYOUT( \
-  KC_ESC,  KC_F1,   KC_F2,   KC_F3, KC_F4,   KC_F5,                                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  _______, KC_PSLS, KC_PMNS, KC_P7, KC_P8,   KC_P9,                                        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_F12, \
-  _______, KC_PAST, KC_PPLS, KC_P4, KC_P5,   KC_P6,                                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
-  _______, KC_PDOT, KC_P0,   KC_P1, KC_P2,   KC_P3,   TG(_LOWER),  _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-                                    _______, KC_LGUI, _______,                    XXXXXXX, KC_CLCK, KC_NLCK\
+  _______,  KC_F1,   KC_F2,   KC_F3, KC_F4,   KC_F5,                                          KC_F6,   KC_F7, KC_PAST, KC_F9,   KC_F10,  KC_F11, \
+  TG(_LOWER), KC_PSLS, KC_PMNS, KC_P7, KC_P8,   KC_P9,                                        KC_PMNS, KC_P7, KC_P8, KC_P9, KC_PERC, KC_F12, \
+  _______, KC_PAST, KC_PPLS, KC_P4, KC_P5,   KC_P6,                                           KC_PPLS, KC_P4, KC_P5, KC_P6, KC_RPRN, KC_LPRN, \
+  _______, KC_PDOT, KC_P0,   KC_P1, KC_P2,   KC_P3,   TG(_LOWER),  _______, _______, _______, KC_PSLS, KC_P1, KC_P2, KC_P3, KC_PDOT, KC_PIPE, \
+                                    _______, KC_LGUI, _______,                    XXXXXXX, KC_PEQL, KC_P0                                              \
 
   ),
 
