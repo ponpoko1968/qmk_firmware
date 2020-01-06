515f2e2b7f04769f5b41625834fc90c959f181fd
shuji ochi <ponpoko1968@gmail.com>
2020-01-06 13:49:08 +0900

    lowerをトグルではなく、テンポラリ（押している間だけ）に変更、左の再下段右端をシフトキーに。

--- keyboards/lily58/keymaps/ponpoko1968/keymap.c
+++ keyboards/lily58/keymaps/ponpoko1968/keymap.c
@@ -54,8 +54,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,   KC_B, LCTL(KC_RBRACKET),  TGL_KANA, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                    ADJUST, KC_LALT,   KC_LGUI, LOWER,                                TGL_KANA, LT(_RAISE,KC_SPACE),   KC_BSPC, KC_RGUI \
+  KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,   KC_B, LOWER,       LCTL(KC_RBRACKET), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+                         ADJUST, KC_LALT,   KC_LGUI, KC_LSFT,           TGL_KANA, LT(_RAISE,KC_SPACE),KC_RCTL, KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
