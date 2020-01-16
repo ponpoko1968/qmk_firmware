8bde417a4626324b99f45513b5a80621edb6bd00
shuji ochi <ponpoko1968@gmail.com>
2020-01-16 22:27:50 +0900

    lowerのテンキー周りを変更

--- keyboards/lily58/keymaps/ponpoko1968/keymap.c
+++ keyboards/lily58/keymaps/ponpoko1968/keymap.c
@@ -52,21 +52,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,   KC_B, LOWER,       LCTL(KC_RBRACKET), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                         ADJUST, KC_LALT,   KC_LGUI, KC_LSFT,           TGL_KANA, LT(_RAISE,KC_SPACE),KC_RCTL, KC_RGUI \
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,   KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,   KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_LCTRL,  KC_Z,   KC_X,   KC_C,    KC_V,   KC_B, LCTL(KC_RBRACKET),      LOWER,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+  /*                   */TG(_LOWER), KC_LALT,  KC_LGUI, KC_LSFT, TGL_KANA, LT(_RAISE,KC_SPACE),KC_RCTL, ADJUST \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |                    |      |      |      |      |      | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |   7  |   8  |   9  |   (  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
+ * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   4  |   5  |   6  |   )  |   -  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
+ * |      |      |      |      |      |      |-------|    |-------|      |   1  |   2  |   3  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -74,10 +74,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11,  KC_F12, \
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_MINUS,KC_7,    KC_8,    KC_9,   KC_RPRN, _______, \
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_PLUS, KC_4,    KC_5,    KC_6,   KC_LPRN, _______, \
-  _______, _______, _______, _______, _______, _______, _______, KC_EQL,  KC_SLSH, KC_1,    KC_2,    KC_3,    XXXXXXX, XXXXXXX, \
-                             _______, _______, _______, _______, KC_ASTR, KC_0,    KC_DOT,_______  \
+  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_MINUS,KC_7,    KC_8,    KC_9,   KC_LPRN, _______, \
+  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_PLUS, KC_4,    KC_5,    KC_6,   KC_RPRN, _______, \
+  _______, _______, _______, _______, _______, _______, _______, KC_EQL,  KC_SLSH, KC_1,    KC_2,    KC_3,   XXXXXXX, XXXXXXX, \
+  /*                    */TG(_LOWER), _______, _______, _______, KC_ASTR, LT(_RAISE,KC_0),  _______, KC_DOT \
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -97,8 +97,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
                   KC_TILD , KC_EXLM  , KC_AT    ,  KC_HASH   , KC_DLR  ,  KC_PERC ,                    KC_CIRC   , KC_AMPR , KC_ASTR  , KC_LPRN , KC_RPRN   , _______   , \
                   _______ , KC_HOME ,  KC_UP ,     KC_EXLM   , KC_AT   ,  KC_TILDE,                    KC_MINS  ,  KC_EQL  , KC_LBRC ,  KC_RBRC , KC_BSLASH , KC_F12    , \
-                  _______ , KC_LEFT,   KC_DOWN,    KC_RIGHT,KC_DLR ,   KC_PERC ,                    KC_UNDS ,   KC_PLUS , KC_LCBR  , KC_RCBR , KC_COLN   , KC_PIPE    , \
-                  _______ , KC_END,    KC_LT,      KC_GT,      KC_HASH,   KC_GRAVE, KC_PGUP, _______,  KC_QUOT ,   KC_DQT  , KC_LT  ,   KC_GT   , KC_QUES ,   _______ , \
+                  _______ , KC_LEFT,   KC_DOWN,    KC_RIGHT,SGUI(KC_TAB), LGUI(KC_TAB) ,               KC_UNDS ,   KC_PLUS , KC_LCBR  , KC_RCBR , KC_COLN   , KC_PIPE    , \
+                  _______ , KC_END,    KC_LT,      KC_GT,      KC_HASH,   KC_GRAVE, KC_PGUP, KC_BSPC,  KC_QUOT ,   KC_DQT  , KC_LT  ,   KC_GT   , KC_QUES ,   _______ , \
                                                          _______, _______, _______, KC_PGDN, _______,  _______, _______, _______\
 ),
 /* ADJUST
@@ -173,8 +173,8 @@ void matrix_render_user(struct CharacterMatrix *matrix) {
     //matrix_write_ln(matrix, read_host_led_state());
     //matrix_write_ln(matrix, read_timelog());
   } else {
-    //matrix_write(matrix, read_logo());
-    matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
+    matrix_write(matrix, read_logo());
+    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
   }
 }
 
